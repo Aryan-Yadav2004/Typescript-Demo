1688336bcce405e24016f5458c0d7d9d20da8a30
Aryan Yadav <yadavaryan122004@gmail.com>
2025-12-28 16:26:16 +0530

    Add type vs interface
    Define custom types and interfaces for arrays and tuples.

---
+++ typeVSinterface.ts
@@ -0,0 +1,17 @@
+// type vs interface
+
+type text = string;
+
+// for defining custorm types for arrays, type keyword is more easy 
+type stringArray = string[];
+
+//using an interface we will define an object which will be always having keys
+//of type number and value to be of the type of the array
+interface numberArray {
+    [index:number] : number
+}
+
+
+//let's say we want to define a pair or a triplet or a custome tupe (set of x values)
+type pair = [number, number];
+type triplet = [number, number, number];
